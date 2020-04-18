e93811f11a8228ce359855ee77f8d6babf6a049a
ash-codemotion <ash@codemotion.eu>
2020-04-18 15:02:42 +0300

    Add using doetenv configuration;

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,8 @@
-export const GOOGLE_API_KEY = 'AIzaSyDXSWeE0i_kjDMez-Egz4lIy2NQ5EJWCCE';
-export const OPENWEATHER_API_KEY = 'b04e545dfbfe13494c5034cafc30dd90';
+import Config from 'react-native-config';
+
+export const GOOGLE_API_KEY = Config.GOOGLE_API_KEY;
+export const OPENWEATHER_API_KEY = Config.OPENWEATHER_API_KEY;
+
 export const FORECAST_DAYS_AMOUNT = 16;
 export const DAYS_TO_CHECK_WASH_FORECAST = 2;
 export const UNIT_SYSTEM = 'metric';
